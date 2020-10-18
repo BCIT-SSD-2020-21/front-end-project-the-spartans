33d5335eae0679605629cbb00a75a8f668575853
abhishekpundir789 <apundir@algomau.ca>
2020-10-17 21:17:25 -0700

    added files

---
+++ Javascript/signup.js
@@ -0,0 +1,9 @@
+const labels = decument.querySelectorAll("label");
+
+labels.forEach((label)=>{
+    label.innerHTML = label.innerText
+    .split(" ")
+    .map((letter,index)=>{
+        return `<span style = "transition-delay:${index*100}ms">${letter}</span>`
+    }).join("");
+})
