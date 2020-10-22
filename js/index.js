17c557c6ca798ea825b70be63573f9b31890ed67
Anthony Chan <achan437@my.bcit.ca>
2020-10-22 09:41:05 -0700

    incorporated localStorage

--- js/index.js
+++ js/index.js
@@ -1,28 +1,27 @@
-Cart = [];
-
 //constructor to create product object with the name, price, img and short description
 function product(name, price,img,){
      this.name = name;
      this.price = price;
      this.img = img;
-}
+};
 
-//creation of cart array to hold products
-     Cart.push(new product("iphone", 1200,"some img"))
-     Cart.push(new product("note20 ultra", 1400, "some other img"))
+Cart =[
+];
+
+localStorage.setItem("Cart",JSON.stringify(Cart));
 
 //when add to car button is pressed on product page, use add cart to grab all info of product and store into new product object and push into cart
-$(".add2cart-btn").on("click", function addCart(Cart){
+$(".add2cart-btn").on("click", function addCart(){
+     var Cart = JSON.parse(localStorage.getItem("Cart"));
      //will grab info needed for the product object from page
      var name = document.getElementById("product-name").innerHTML;
      var price = document.getElementById("product-price").innerHTML;
      var img = document.getElementById("product-img").src;
-     
      //get item from which add to cart button was clicked
      var item = new product(name,price,img);
-     //Cart.push(item);
-     console.log("Item info be: "+item.name+" "+item.price+", "+item.img)
-})
+     Cart.push(item);
+     localStorage.setItem("Cart",JSON.stringify(Cart));
+});
 
 //display everything in cart
 function listCart(Cart){
@@ -30,7 +29,7 @@ function listCart(Cart){
           //use cart.imgUrl and cart.price to display and some html to format
 
      }
-}
+};
 
 //calculate total of everything in cart
 function calcTotal(Cart){
@@ -39,7 +38,7 @@ function calcTotal(Cart){
           total += Cart.price[i];
      }
      return total;
-}
+};
 
 $("checkout-button").on("click",function(){
      
