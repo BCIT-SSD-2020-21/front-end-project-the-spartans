b43449b53e06a04f13ef99966388caeb1a954611
Anthony Chan <achan437@my.bcit.ca>
2020-10-24 13:51:49 -0700

    integrated localStorage, added checkout page links

--- js/index.js
+++ js/index.js
@@ -5,61 +5,68 @@ function product(name, price,img,){
      this.img = img;
 };
 
-Cart =[
-     new product("iphone", 1200, "no img"),
-     new product("galaxy s20", 1000, "no img")
-];
+function initCart(){
+     var Cart=[];
+     var test =localStorage .getItem("Cart"); 
 
-localStorage.setItem("Cart",JSON.stringify(Cart));
+     if(test !== null){
+          Cart = JSON.parse(localStorage.getItem("Cart")); 
+     }else{
+          localStorage.setItem("Cart",JSON.stringify(Cart));
+     }
+}
+
+function clearCart(){
+     //remove all items from cart
+     localStorage.removeItem("Cart");
+     location.reload();
+}
 
 //when add to car button is pressed on product page, use add cart to grab all info of product and store into new product object and push into cart
-$(".add2cart-btn").on("click", function addCart(){
+$(".add2cart-btn").on("click", function(){
+     initCart();
+
      var Cart = JSON.parse(localStorage.getItem("Cart"));
      //will grab info needed for the product object from page
      var name = document.getElementById("product-name").innerHTML;
-     var price = document.getElementById("product-price").innerHTML;
+     var temp = document.getElementById("product-price").dataset.price;
      var img = document.getElementById("product-img").src;
+     var price = parseFloat(temp);
+
      //get item from which add to cart button was clicked
      var item = new product(name,price,img);
      Cart.push(item);
+     console.log("added item to list")
      localStorage.setItem("Cart",JSON.stringify(Cart));
+     
 });
-
-//calculate total of everything in cart
-function calcTotal(){
-     var Cart = JSON.parse(localStorage.getItem("Cart"));
-     var total = 0;
-     for(var i = 0; i < Cart.length; i++){
-          total += Cart[i].price;
-     }
-     return total;
-};
 
 //display everything in cart
 //https://stackoverflow.com/questions/17773938/add-a-list-item-through-javascript
 function listCart(){
      var Cart = JSON.parse(localStorage.getItem("Cart"));
-     for(var i = 0; i < Cart.length; i++){
-          //use cart.imgUrl and cart.price to display and some html to format
-          var list = document.getElementById('product-list');
-          var entry = document.createElement('li');
-          entry.appendChild(document.createTextNode(Cart[i].name));
-          list.appendChild(entry);
+     if (!Cart){
+          document.getElementById('total').innerHTML =  "no item in cart";
+     }else{
+          var total = 0;
+          
+          for(var i = 0; i < Cart.length; i++){
+               total += Cart[i].price;
+               //use cart.imgUrl and cart.price to display and some html to format
+               var list = document.getElementById('product-list');
+               var entry = document.createElement('li');
+               entry.appendChild(document.createTextNode(Cart[i].name));
+               list.appendChild(entry);
 
-          var list = document.getElementById('product-price');
-          var entry = document.createElement('li');
-          entry.appendChild(document.createTextNode(Cart[i].price));
-          list.appendChild(entry);
+               var list = document.getElementById('product-price');
+               var entry = document.createElement('li');
+               entry.appendChild(document.createTextNode(Cart[i].price));
+               list.appendChild(entry);
+          }
+          document.getElementById('total').innerHTML = "Total: $" + total;
      }
-     
-     document.getElementById('total').innerHTML = "Total: $" + calcTotal();
-
 };
 
-
-
-$(".cart-btn").on("click", listCart());
-
-$("checkout-button").on("click",function(){
+$(".checkout-button").on("click",function(){
      
 });
