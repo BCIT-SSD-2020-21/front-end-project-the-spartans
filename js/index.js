8a012f1c5dbc80769df1e2f09fc757918ba25962
Anthony Chan <achan437@my.bcit.ca>
2020-10-24 18:47:56 -0700

    added remove item functionality #9

--- js/index.js
+++ js/index.js
@@ -1,13 +1,14 @@
 //constructor to create product object with the name, price, img and short description
-function product(name, price,img,){
+function product(name, price,img,itemNum){
      this.name = name;
      this.price = price;
      this.img = img;
+     this.itemNum = itemNum;
 };
 
 function initCart(){
      var Cart=[];
-     var test =localStorage .getItem("Cart"); 
+     var test =localStorage.getItem("Cart"); 
 
      if(test !== null){
           Cart = JSON.parse(localStorage.getItem("Cart")); 
@@ -27,6 +28,7 @@ $(".add2cart-btn").on("click", function(){
      initCart();
 
      var Cart = JSON.parse(localStorage.getItem("Cart"));
+     var count = Cart.length;
      //will grab info needed for the product object from page
      var name = document.getElementById("product-name").innerHTML;
      var temp = document.getElementById("product-price").dataset.price;
@@ -34,7 +36,7 @@ $(".add2cart-btn").on("click", function(){
      var price = parseFloat(temp);
 
      //get item from which add to cart button was clicked
-     var item = new product(name,price,img);
+     var item = new product(name,price,img,count);
      Cart.push(item);
      console.log("added item to list")
      localStorage.setItem("Cart",JSON.stringify(Cart));
@@ -52,20 +54,37 @@ function listCart(){
           
           for(var i = 0; i < Cart.length; i++){
                total += Cart[i].price;
-               //use cart.imgUrl and cart.price to display and some html to format
+               //use cart.img and cart.price to display and some html to format
                var list = document.getElementById('product-list');
-               var entry = document.createElement('li');
+               var entry = document.createElement('dt');
                entry.appendChild(document.createTextNode(Cart[i].name));
                list.appendChild(entry);
 
+               var removeButton = document.createElement("button");
+               removeButton.innerHTML= "Remove Item";
                var list = document.getElementById('product-price');
-               var entry = document.createElement('li');
-               entry.appendChild(document.createTextNode(Cart[i].price));
+               var entry = document.createElement('dt');
+               entry.appendChild(document.createTextNode(Cart[i].price.toFixed(2)));
                list.appendChild(entry);
+               list.appendChild(removeButton);
+
+               $("button").addClass("remove-btn"+i);
+               for(j=i-1;j>=0;j--){
+                    $(".remove-btn"+j).removeClass("remove-btn"+i);
+               }
+               $(".checkout-button").removeClass("remove-btn"+i);
+               $(".clear-button").removeClass("remove-btn"+i);
+
+               $(".remove-btn"+i).on("click",function(i){
+                    Cart.splice(i,1);
+                    localStorage.setItem("Cart",JSON.stringify(Cart));
+                    location.reload();
+               });
           }
-          document.getElementById('total').innerHTML = "Total: $" + total;
+          document.getElementById('total').innerHTML = "Total: $" + total.toFixed(2);
      }
 };
+
 
 $(".checkout-button").on("click",function(){
      
